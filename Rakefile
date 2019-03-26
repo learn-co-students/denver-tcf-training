96bdfb69d7ec0a71f29f8519b527904fc73b95c3
Tim Campbell <timothylevicampbell@gmail.com>
2019-03-26 17:11:34 -0600

    Finish manual ORM

---
+++ Rakefile
@@ -0,0 +1,10 @@
+require_relative './config/environment'
+
+def reload
+  load './config/environment.rb'
+end
+
+desc "Open a new console"
+task :console do
+  Pry.start # start pry with some environment
+end
---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+# require 'pry'
+# require 'sqlite3'
+# require_relative '../lib/dog.rb'
+
+require 'bundler/setup'
+Bundler.require
+
+# load my files
+# require_rel '../lib'
+require_all 'lib'
+
+DB = SQLite3::Database.new('db/test.sqlite3')
+DB.results_as_hash = true
